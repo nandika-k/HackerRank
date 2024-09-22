b9e3178d509fa4d2cb5369bf17fb5ff515f3113b
Nandika K <2nandika.k@gmail.com>
2024-09-22 12:31:47 -0400

    format update

--- PlusMinus.cpp
+++ PlusMinus.cpp
@@ -1,8 +1,4 @@
-// # include <bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -18,10 +14,10 @@ void plusMinus(vector<int> arr) {
 
     for (int num: arr) {
         if (num > 0) {
-                plus++;
+            plus++;
         }
         else if (num == 0) {
-            zero = 1;
+            zero++;
         }
         else {
             minus++;
@@ -29,6 +25,70 @@ void plusMinus(vector<int> arr) {
     }
     cout << fixed << setprecision(6);
     cout << plus / length << endl;
-    cout << zero / length << endl;
     cout << minus / length << endl;
+    cout << zero / length << endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
